3929dc15cf7877089332d7a43301a438d1f29475
roran williams <126867403+roran-williams@users.noreply.github.com>
2024-03-17 15:15:55 +0000

    start using configuring blog pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,10 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+import FeaturePost from "@/app/components/feature-post" 
+
+ const Blog = ({ params }: { params: { slug: string } })=> {
+  return (<div>
+    My Post: {params.slug}
+    <FeaturePost topic={params.slug} />
+  </div>);
 }
+
+export default Blog
